8411bd54b629e492c6cf21aa64d1f603f6e9061d
nullcss <nullyl@163.com>
2017-11-13 17:03:06 +0800

    css

--- js/index.js
+++ js/index.js
@@ -76,27 +76,6 @@ var swiper = new Swiper('.swiper-container1', {
     }
 });
 
-// 动态数字
-var options = {
-    useEasing: true,
-    useGrouping: true,
-    separator: ',',
-    decimal: '.',
-    prefix: '',
-    suffix: ''
-};
-var demo = new CountUp("myTargetElement", 0, 120000, 0, 2.5, options);
-var demo2 = new CountUp("myTargetElement2", 0, 3000, 0, 2.5, options);
-var demo3 = new CountUp("myTargetElement3", 0, 900, 0, 2.5, options);
-var demo4 = new CountUp("myTargetElement4", 0, 8300, 0, 2.5, options);
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 1000) {
-        demo.start();
-        demo2.start();
-        demo3.start();
-        demo4.start();
-    }
-});
 
 // 特色课程部分pc端
 $('.schoolday-lis>li').mouseover(function () {
@@ -141,3 +120,42 @@ AOS.init({
     duration: 300,
     easing: 'ease-in-sine',
   });
+
+// 返回顶部
+function WeixinTop(){
+	this.init();
+}
+WeixinTop.prototype = {
+	constructor: WeixinTop,
+	init: function(){		
+		this._initBackTop();
+	},	
+	_initBackTop: function(){
+		var $backTop = this.$backTop = $('<div class="cbbfixed">'+
+						'<a class="weixin cbbtn"">'+
+							'<span class="weixin-icon"></span><div></div>'+
+						'</a>'+
+						'<a class="gotop cbbtn">'+
+							'<span class="up-icon"></span>'+
+						'</a>'+
+					'</div>');
+		$('body').append($backTop);
+		
+		$(".gotop").click(function(){
+			$("html, body").animate({
+				scrollTop: 0
+			}, 800);
+		});
+		var timmer = null;
+		$(window).bind("scroll",function() {
+            var d = $(document).scrollTop(),
+            e = $(window).height();
+            0 < d ? $backTop.css("bottom", "20px") : $backTop.css("bottom", "-90px");
+			clearTimeout(timmer);
+			timmer = setTimeout(function() {
+                clearTimeout(timmer)
+            },100);
+	   });
+	}
+}
+var WeixinTop = new WeixinTop();
